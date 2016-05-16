140373ced5c3f871f4d3390dd183e9f62ab603af
Chris Benoit <christopher.benoit@rbc.com>
2016-05-15 23:35:39 -0400

    Initial Commit

---
+++ FrameworkTest/FrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTest.h
+//  FrameworkTest
+//
+//  Created by Chris Benoit on 2016-05-15.
+//  Copyright © 2016 RBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkTest.
+FOUNDATION_EXPORT double FrameworkTestVersionNumber;
+
+//! Project version string for FrameworkTest.
+FOUNDATION_EXPORT const unsigned char FrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest/PublicHeader.h>
+
+
