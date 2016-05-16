cd11ff222b4545abf1358925516ab60033185b55
Chris Benoit <christopher.benoit@rbc.com>
2016-05-16 00:04:39 -0400

    more

---
+++ FrameworkTest/TestClass.h
@@ -0,0 +1,14 @@
+//
+//  TestClass.h
+//  FrameworkTest
+//
+//  Created by Chris Benoit on 2016-05-15.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
++ (void) doStuff;
+
+@end
---
+++ FrameworkTest/TestClass.m
@@ -0,0 +1,18 @@
+//
+//  TestClass.m
+//  FrameworkTest
+//
+//  Created by Chris Benoit on 2016-05-15.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
++ (void) doStuff
+{
+    NSLog(@"Doing stuff!");
+}
+
+
+@end
